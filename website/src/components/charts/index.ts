5ddb5308e85e4d05226a3485884e2d81cbd1eeb7
jksiravo2323 <116385131+jksiravo2323@users.noreply.github.com>
2026-06-23 00:11:10 +0200

    Add chart components and BaseLayout

---
+++ website/src/components/charts/index.ts
@@ -0,0 +1,6 @@
+export { default as BarChart } from "./BarChart.astro";
+export { default as LineChart } from "./LineChart.astro";
+export { default as Heatmap } from "./Heatmap.astro";
+export { default as GeoTileMap } from "./GeoTileMap.astro";
+export { default as StatRow } from "./StatRow.astro";
+export { default as Figure } from "./Figure.astro";
